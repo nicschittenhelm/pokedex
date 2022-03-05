d0c6cd62d6f53ddfdcfa202b01085e3cf7b02f74
ebrofi <ebrofi@gmail.com>
2022-03-05 02:58:54 +0100

    installed tailwind plugin to render images without antialising

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Card = ({pokemon}) => {
     return (
-        <div className="relative bg-slate-400 w-64 h-72 rounded-md drop-shadow-md p-2 m-5">
+        <div className="relative bg-blue-500 w-64 h-72 rounded-md drop-shadow-md p-2 m-5">
             <h2 className="text-3xl text-white capitalize font-bold tracking-wide">{pokemon.name}</h2>
             <p>{pokemon.id}</p>
-            <img src={pokemon.sprites.front_default} alt={pokemon.name} className="absolute w-full -bottom-10 -right-10"/>
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} className="absolute w-full -bottom-10 -right-10 rendering-pixelated"/>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,10 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  variants: { // all the following default to ['responsive']
+    imageRendering: ['responsive'],
+  },
+  plugins: [
+    require('tailwindcss-image-rendering')(),
+  ],
 }
