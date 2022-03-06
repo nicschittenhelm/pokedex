299c472325dacc241760a4284f62911f359b00ea
ebrofi <ebrofi@gmail.com>
2022-03-06 02:33:02 +0100

    got rid of search function at first and implemented full listview first. worked on styling, basic implementation of gradiant based on type working

--- src/App.js
+++ src/App.js
@@ -2,32 +2,46 @@ import React from "react"
 import { useEffect, useState } from "react"
 import Card from "./Card"
 
-const URL_SEARCH = "https://pokeapi.co/api/v2/pokemon/"
-//const URL_LIST = "https://pokeapi.co/api/v2/pokemon?limit=152"
+const URL = "https://pokeapi.co/api/v2/pokemon/"
 
 const App = () => {
 
     const [pokemon, setPokemon] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    const searchPokedex = async (name) => {
-        const response = await fetch(`${URL_SEARCH}${name}`)
-        const data = await response.json();
-        setPokemon(data)
+    const fillPokedex = async () => {
+        setLoading(true);
+        let pokedex = [];
+        for (let i = 1; i <= 151; i++) {
+            pokedex.push(await getPokemonData(i));
+        }
+        setPokemon(pokedex);
+        setLoading(false);
     }
 
+    const getPokemonData = async (id) => {
+        const response = await fetch(`${URL}${id}`);
+        const data = await response.json();
+        return data;
+    }
+    
     useEffect(() => {
-        searchPokedex("poliwrath")
+        fillPokedex();
     }, []);
 
     return (
+        loading ? <p>loading ...</p> : (
         <>
-        <h1 className="bg-black text-red-600">Pokedex</h1>
-        <div className="flex">
-          <Card pokemon={pokemon} />
-          <Card pokemon={pokemon} />
-          <Card pokemon={pokemon} />
+        <h1 className="text-red-600 text-center my-10 text-8xl font-bold">Pokédex</h1>
+        <div className="flex flex-row min-h-screen justify-center items-center">
+            <div className="rounded-md drop-shadow-xl bg-white w-[900px] flex flex-wrap justify-center">
+                {pokemon.map(p => (
+                <Card pokemon={p} />
+                ))}
+            </div>
         </div>
         </>
+        )
     );
 }
 
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,13 +1,60 @@
 import React from "react";
+import { useEffect, useState } from "react"
+
+var typeColors = {
+    'normal' : '#A8A77A',
+    'fire' : '#EE8130',
+    'water' : '#6390F0',
+    'electric' : '#F7D02C',
+    'grass' : '#7AC74C',
+    'ice' : '#96D9D6',
+    'fighting' : '#C22E28',
+    'poison' : '#A33EA1',
+    'ground' : '#E2BF65',
+    'flying' : '#A98FF3',
+    'psychic' : '#F95587',
+    'bug' : '#A6B91A',
+    'rock' : '#B6A136',
+    'ghost' : '#735797',
+    'dragon' : '#6F35FC',
+    'dark' : '#705746',
+    'steel' : '#B7B7CE',
+    'fairy' : '#D685AD',
+};
+
 
 const Card = ({pokemon}) => {
+
+
+    let colorA = "#FF0000";
+    let colorB = "#0000FF";
+
+    //console.log(typeColors[pokemon.types[0].type.name])
+
+    // this simple check doesnt work properly. always print only one of two logs. works when hotreloading, usually some problem with data not fully loaded.
+    if (pokemon.types.length = 0) {
+        console.log("two")
+    } else {
+        console.log("one")
+    }
+    
+
+
+
+
     return (
-        <div className="relative bg-blue-500 w-64 h-72 rounded-md drop-shadow-md p-2 m-5">
-            <h2 className="text-3xl text-white capitalize font-bold tracking-wide">{pokemon.name}</h2>
-            <p>{pokemon.id}</p>
+        <div className="bg-black h-80 w-60 rounded-md drop-shadow-md p-2 m-5" style={{background:`linear-gradient(150deg, ${colorA} 0%, ${colorB} 100%)`}}>
+            <h2 className="text-3xl text-white capitalize font-bold">{pokemon.name}</h2>
+
+                {pokemon.types.map(p => (
+                    <p key={p.type.name}>{p.type.name}</p>
+                ))}
+
+
             <img src={pokemon.sprites.front_default} alt={pokemon.name} className="absolute w-full -bottom-10 -right-10 rendering-pixelated"/>
         </div>
     )
 }
 
 export default Card
+
